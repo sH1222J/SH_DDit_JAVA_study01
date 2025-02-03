a45d40faa00a2d10de942eec759c22f4216fafd8
DESKTOP-6UTLHP2\PC-07 <a@a>
2025-02-03 17:17:08 +0900

    20250203 JAVA 수업1

---
+++ workspace/.metadata/.plugins/org.eclipse.core.resources/.history/67/a0452573fee1001f1bc7cfbea21dded2
@@ -0,0 +1,9 @@
+package kr.or.ddit.study01;
+
+public class DocExample {
+		public static void main(String[] args) {
+			
+			System.out.println("주석 TEST");
+		}
+
+}
--- workspace/BasicJava/src/kr/or/ddit/study01/HelloWorld.java
+++ workspace/BasicJava/src/kr/or/ddit/study01/HelloWorld.java
@@ -4,6 +4,6 @@ import java.security.DomainCombiner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("SH");
+		System.out.println("정성호");
 	}
 }
