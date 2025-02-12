a2598a5fe836fea8837de80e1eb26638347e5646
sH. <28824568+sH1222J@users.noreply.github.com>
2025-02-12 17:37:26 +0900

    20250212 스택 및 배열 관련 수업 및 숙제

---
+++ workspace/BasicJava/src/kr/or/ddit/study05/sec01/ReferenceExample03.java
@@ -0,0 +1,32 @@
+package kr.or.ddit.study05.sec01;
+
+import java.util.Scanner;
+
+public class ReferenceExample03 {
+	Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		ReferenceExample03 obj = new ReferenceExample03();
+		obj.process();
+
+	}
+
+	public void process() {
+		int a = 10;
+		if (a == 10) {
+			int sum = 0;
+			for (int i = 1; i <= 3; i++) {
+				String str = new String("abc");
+				sum += i;
+			}
+			int b = 20;
+
+			String str = "abc";
+		}
+		int sum = 0;
+		int i = 2;
+		for (; i < 3; i++) {
+			sum += i;
+		}
+	}
+}
